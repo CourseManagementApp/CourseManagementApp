b4bd5f059be1fa1b94c04f39fd5329ba52f712e8
James <35617601+jliby@users.noreply.github.com>
2023-02-04 13:29:01 -0500

    ui

---
+++ my-app/src/components/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore"
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDbREFe9__OIzrs5N0KvSvDPJlfe-PYV-Y",
+  authDomain: "ece-database.firebaseapp.com",
+  databaseURL: "https://ece-database-default-rtdb.firebaseio.com",
+  projectId: "ece-database",
+  storageBucket: "ece-database.appspot.com",
+  messagingSenderId: "501884735282",
+  appId: "1:501884735282:web:b6b5dc88e8c7de3ea0512b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const firestore = getFirestore(app)
---
+++ my-app/src/dashboard/Orders.js
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import Link from '@mui/material/Link';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Title from './Title';
+
+// Generate Order Data
+function createData(id, date, name, shipTo, paymentMethod, amount) {
+  return { id, date, name, shipTo, paymentMethod, amount };
+}
+
+const rows = [
+  createData(
+    0,
+    '16 Mar, 2019',
+    'John Doug',
+    'N/A',
+    'N/A',
+    14.00,
+  )];
+
+function preventDefault(event) {
+  event.preventDefault();
+}
+
+export default function Orders() {
+  return (
+    <React.Fragment>
+      <Title></Title>
+      <Table size="small">
+        <TableHead>
+          <TableRow>
+            <TableCell>Date</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell>Position</TableCell>
+            <TableCell>Dept.</TableCell>
+            <TableCell align="right">Salary</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow key={row.id}>
+              <TableCell>{row.date}</TableCell>
+              <TableCell>{row.name}</TableCell>
+              <TableCell>{row.shipTo}</TableCell>
+              <TableCell>{row.paymentMethod}</TableCell>
+              <TableCell align="right">{`$${row.amount}`}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      <Link color="primary" href="#" onClick={preventDefault} sx={{ mt: 3 }}>
+        Expand
+      </Link>
+    </React.Fragment>
+  );
+}
